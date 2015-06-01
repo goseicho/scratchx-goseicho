01ce47d83f9cf3cba4c59d5ca597eba5be154a4e
Kazunari Ito <ito.kazunari@gmail.com>
2015-06-01 17:01:00 +0900

    use Audio class

--- goseicho.js
+++ goseicho.js
@@ -10,28 +10,9 @@
     // blockが呼び出された時に呼ばれる関数を登録する。
     // 下にあるdescriptorでブロックと関数のひも付けを行っている。
     ext.goseicho = function(str) {
-        loadSound("http://goseicho.com/goseicho1.mp3")
+       var audio =  new Audio("http://goseicho.com/goseicho1.mp3");
+       audio.play();
     };
-
-   var buffer = null;
-   // Fix up prefixing
-   window.AudioContext = window.AudioContext || window.webkitAudioContext;
-   var context = new AudioContext();
-
-function loadSound(url) {
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.responseType = 'arraybuffer';
-
-  // Decode asynchronously
-  request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
-    }, onError);
-  }
-  request.send();
-}
-
-
 
     // ブロックと関数のひも付け
     var descriptor = {
